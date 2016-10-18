933e553d6c957878d94d7090d5d46ae3f3f1b396
Hrushikesh Dhumal <hrushikesh.dhumal@gmail.com>
2016-10-18 00:56:56 -0400

    changed server from local to network

--- server/mongo.py
+++ server/mongo.py
@@ -84,5 +84,5 @@ def get_data_in_bounding_box():
   return json.dumps(output, sort_keys=True, indent=2)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    # app.run(debug=True)
+    app.run(host= '0.0.0.0', threaded=True, debug=False)
